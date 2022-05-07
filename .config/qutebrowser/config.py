faccdc90ba860cbaf415800ca82e0547c95c87d9
GaahDias <gabrielhdias148@gmail.com>
2022-05-06 21:05:49 -0300

    Changed qutebrowser config

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -25,18 +25,22 @@ c.url.searchengines = {
 
 # Start page
 c.url.start_pages = ['https://google.com']
+c.url.default_page = 'https://google.com'
 
 # Dark mode
-c.colors.webpage.preferred_color_scheme = 'dark'
+# c.colors.webpage.preferred_color_scheme = 'dark'
 c.colors.webpage.darkmode.enabled = True
 c.colors.webpage.darkmode.algorithm = 'lightness-hsl'
-c.colors.webpage.darkmode.grayscale.all = True
-c.colors.webpage.darkmode.contrast = 0.35
+c.colors.webpage.darkmode.contrast = 0.08
+c.colors.webpage.darkmode.threshold.background = 65
+
+# Stylesheet theme
+# https://github.com/alphapapa/solarized-everything-css
+c.content.user_stylesheets = ['~/.themes/draculized/draculized-all-sites.css']
 
 # Key binds
 config.bind('<Ctrl-m>', 'open qute://bookmarks/')
 config.bind('<Ctrl-=>', 'bookmark-add')
-config.bind('<Ctrl-Shift-=', 'bookmark-del')
 
 # Font theme
 primary_font   = 'Sans'
@@ -53,12 +57,13 @@ c.fonts.statusbar = f'11pt "{secundary_font}"'
 primary_main_color   = '#85A4F3'
 secundary_main_color = '#8D91F6'
 primary_bg_color     = '#1A1919'
-secundary_bg_color   = '#454343'
+secundary_bg_color   = '#333232'
 primary_fg_color     = '#E7E1E1'
 secundary_fg_color   = '#CAC5C5'
 
+c.colors.webpage.bg = secundary_bg_color
 c.colors.completion.fg = secundary_fg_color
-c.colors.completion.odd.bg = primary_bg_color
+c.colors.completion.odd.bg = secundary_bg_color
 c.colors.completion.even.bg = primary_bg_color
 c.colors.completion.category.fg = primary_main_color
 c.colors.completion.category.bg = primary_bg_color
@@ -77,11 +82,11 @@ c.colors.statusbar.passthrough.bg = secundary_main_color
 c.colors.statusbar.command.bg = primary_bg_color
 c.colors.statusbar.url.warn.fg = secundary_bg_color
 c.colors.tabs.bar.bg = primary_bg_color
-c.colors.tabs.odd.bg = primary_bg_color
+c.colors.tabs.odd.bg = secundary_bg_color
 c.colors.tabs.even.bg = primary_bg_color
-c.colors.tabs.selected.odd.bg = primary_bg_color
+c.colors.tabs.selected.odd.bg = secundary_bg_color
 c.colors.tabs.selected.even.bg = primary_bg_color
-c.colors.tabs.pinned.odd.bg = primary_bg_color
+c.colors.tabs.pinned.odd.bg = secundary_bg_color
 c.colors.tabs.pinned.even.bg = primary_bg_color
-c.colors.tabs.pinned.selected.odd.bg = primary_bg_color
+c.colors.tabs.pinned.selected.odd.bg = secundary_bg_color
 c.colors.tabs.pinned.selected.even.bg = primary_bg_color
